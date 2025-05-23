f9c4acb6dac0bd67e1f84601633095e3aa59611c
microwalker <microwalker@gmx.net>
2025-05-23 12:00:10 +0200

    added bonus tasksheet

--- lib/input_counter_box.dart
+++ lib/input_counter_box.dart
@@ -1,28 +1,41 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class InputCounterBox extends StatefulWidget {
   final Color? backgroundColor;
-  final void Function(String value)? onChanged; 
+  final void Function(String value)? onChanged;
+  final TextEditingController? controller;
 
-  const InputCounterBox({super.key, this.onChanged, this.backgroundColor});
+  const InputCounterBox({super.key, this.onChanged, this.backgroundColor, this.controller});
 
   @override
   State<InputCounterBox> createState() => _InputCounterBoxState();
+
+  Color getRandomColor() => Color(Random().nextInt(0x7f7f7f) | 0xff808080); 
 }
 
 class _InputCounterBoxState extends State<InputCounterBox> {
   int _length = 0;
+  Color? color;
+
+  @override
+  void initState() {
+    super.initState();
+    color = widget.backgroundColor ?? widget.getRandomColor();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: widget.backgroundColor,
+      color: color,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextField(
+              controller: widget.controller,
               onChanged: (text) {
                 setState(() {
                   _length = text.length;
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,8 +1,20 @@
 import 'package:callback_functions/input_counter_box.dart';
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
+
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int totalLength = 0;
+
+  TextEditingController controller1 = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
+  TextEditingController controller3 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -13,13 +25,27 @@ class MainScreen extends StatelessWidget {
           child: Column(
             spacing: 16,
             children: [
-              InputCounterBox(backgroundColor: Colors.blueAccent.shade200, onChanged: (value) => debugPrint(value),),
-              InputCounterBox(onChanged: (value) => debugPrint(value),),
-              InputCounterBox(onChanged: (value) => debugPrint(value), backgroundColor: Colors.yellowAccent.shade200,),
+              InputCounterBox(onChanged: (value) => refreshTotalLength(), controller: controller1,),
+              InputCounterBox(onChanged: (value) => refreshTotalLength(), controller: controller2,),
+              InputCounterBox(onChanged: (value) => refreshTotalLength(), controller: controller3,),
+              Text("Summe aller Längen: $totalLength")
             ],
           ),
         ),
       ),
     );
+  }
+
+  void refreshTotalLength() {
+    setState(() => totalLength = controller1.text.length + controller2.text.length + controller3.text.length); 
+  }
+
+  @override
+  void dispose() {
+    controller1.dispose();
+    controller2.dispose();
+    controller3.dispose();
+
+    super.dispose();
   }
 }
